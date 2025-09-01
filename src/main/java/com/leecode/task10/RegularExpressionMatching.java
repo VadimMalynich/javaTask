57f83f999b53a513ea8efa42ebe9b7597d09f168
Vadim <vadim.malynich@mail.ru>
2025-09-01 20:35:39 +0300

    Solution of finding regular expression matching

--- src/main/java/com/leecode/task10/RegularExpressionMatching.java
+++ src/main/java/com/leecode/task10/RegularExpressionMatching.java
@@ -12,10 +12,6 @@ import java.util.regex.Pattern;
  */
 public class RegularExpressionMatching {
     public boolean isMatch(String s, String p) {
-        Matcher matcher = Pattern.compile("^" + p + "$").matcher(s);
-        if (matcher.find()) {
-            return true;
-        }
-        return false;
+        return Pattern.compile("^" + p + "$").matcher(s).find();
     }
 }
